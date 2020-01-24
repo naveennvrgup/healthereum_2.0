c1d7a2f5a89ce0d9036713c4cf02cec8b160cda1
naveennvrgup <naveennvrgup@gmail.com>
2020-01-24 19:26:26 +0530

    temp cmt

--- frontend/web/src/App.js
+++ frontend/web/src/App.js
@@ -1,53 +1,42 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import Content from './Components/Content';
 import HospitalDashboard from './Components/Hospital/hospital-app-board';
-import EmergencySearch from './Components/Hospital/emergency-search';
-import RegisterDoctor from './Components/Hospital/register-doctor';
 import DoctorDashboard from './Components/Doctor/doctor-app-board';
 import ViewHistorybyDoctor from './Components/Doctor/doctor-viewhistory';
 import PatientHistory from './Components/Patient/patient-history';
 import blockchain from './blockchain/app';
 import web3 from './blockchain/web3';
 
-async function App() {
-  const accounts = await web3.eth.getAccounts();
-  console.log(accounts)
-  // blockchain.methods.addUser('8940073123', 'naveen sundar', 'naveennvrgup@gmail.com', 1, 1, '17-12-1998')
-  //   .send({from:accounts[0]})
-  //   .then(d => {
-  //     console.log()
-  //   })
 
-  blockchain.methods.getHospitals().call().then(d => {
-    console.log(d)
-  })
 
-  return (
-    <Suspense>
+export default class App extends Component {
+  componentDidMount() {
+    
+  }
+  
+
+  render() {
+    return (
       <BrowserRouter>
         <div className="container">
           <Switch>
             <Route exact path='/' component={Content} />
-            <Route path='/h-dashboard' component={HospitalDashboard} />
-            <Route path='/emergency-search' component={EmergencySearch} />
-            <Route path='/register-doctor' component={RegisterDoctor} />
+            
+            <Route path='/reception' component={HospitalDashboard} />
 
-            <Route path='/d-dashboard' component={DoctorDashboard} />
-            <Route path='/dv-history' component={ViewHistorybyDoctor} />
-            <Route path='/add-record' component={RegisterDoctor} />
-
-            <Route path='/pv-history' component={PatientHistory} />
+            <Route path='/docterDashboard' component={DoctorDashboard} />
+            <Route path='/docterHistory' component={ViewHistorybyDoctor} />
+            <Route path='/patientsHistory' component={PatientHistory} />
 
           </Switch>
 
 
         </div>
       </BrowserRouter>
-    </Suspense>
-  );
+    )
+  }
 }
 
-export default App;
--- frontend/web/src/Components/Navbar.js
+++ frontend/web/src/Components/Navbar.js
@@ -1,41 +1,46 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { MDBContainer, MDBBtn, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter } from 'mdbreact';
 import LoginRegister from './login-register';
 
 
 class Navbar extends Component {
-    state = { 
+    state = {
         modal: false
-     }
+    }
 
     toggle = () => {
         this.setState({
-          modal: !this.state.modal
+            modal: !this.state.modal
         });
-      }
+    }
 
-    render() { 
-        return ( 
-                <header>
-                    <Link href="" className="logo">Health<span>Ereum</span></Link>
-                    <ul>
-                        <li><Link to="/" className="active">Home</Link></li>
-                        <li><Link to="/Home" >Menu</Link></li>
-                        <li><Link to="/About">Contact</Link></li>
-                        <li><Link style={{fontWeight:800,border:"2px solid #78ff00"}} to="#" onClick={this.toggle} >Login/Signup</Link></li>
-                    </ul>
-                    
-                        <MDBModal isOpen={this.state.modal} size="lg" toggle={this.toggle} centered>
-                            <MDBModalBody style={{background:"#333"}}>
-                               <LoginRegister/>
-                            </MDBModalBody>
-                            
-                        </MDBModal>
-                    
-                </header>
-         );
+    render() {
+        return (
+            <header>
+                <Link href="" className="logo">Health<span>Ereum</span></Link>
+                <ul>
+                    <li>
+                        <Link
+                            style={{
+                                fontWeight: 800,
+                                border: "2px solid #78ff00",
+                                color: 'white'
+                            }} to="#" onClick={this.toggle} >Login/Signup
+                        </Link>
+                    </li>
+                </ul>
+
+                <MDBModal isOpen={this.state.modal} size="lg" toggle={this.toggle} centered>
+                    <MDBModalBody style={{ background: "#333" }}>
+                        <LoginRegister />
+                    </MDBModalBody>
+
+                </MDBModal>
+
+            </header>
+        );
     }
 }
- 
+
 export default Navbar;
