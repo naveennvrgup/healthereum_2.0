f8c1c477fe9a524c215b1c64eba0cd7eabdb069a
naveennvrgup <naveennvrgup@gmail.com>
2020-01-26 03:24:58 +0530

    dashboard done

--- frontend/web/src/Components/Hospital/Dashboard.js
+++ frontend/web/src/Components/Hospital/Dashboard.js
@@ -1,20 +1,91 @@
 import React, { Component } from 'react'
-import { Descriptions } from 'antd'
+import {
+    Descriptions, Button, Form,
+    Input,
+    Tooltip,
+    Icon,
+    Cascader,
+    Select,
+    Row,
+    Col,
+    Checkbox,
+    AutoComplete,
+} from 'antd';
 
 export default class Dashboard extends Component {
+    state = {
+        edit: false
+    }
+
     render() {
-        return (
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 8 },
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 16 },
+            },
+        };
+        const tailFormItemLayout = {
+            wrapperCol: {
+                xs: {
+                    span: 24,
+                    offset: 0,
+                },
+                sm: {
+                    span: 16,
+                    offset: 8,
+                },
+            },
+        };
+
+        let editdiv =
             <div>
-                <Descriptions title="User Info">
-                    <Descriptions.Item label="UserName">Zhou Maomao</Descriptions.Item>
-                    <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
-                    <Descriptions.Item label="Live">Hangzhou, Zhejiang</Descriptions.Item>
-                    <Descriptions.Item label="Remark">empty</Descriptions.Item>
-                    <Descriptions.Item label="Address">
-                        No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
-    </Descriptions.Item>
-                </Descriptions>
+
             </div>
+
+        let noneditdiv =
+            <div>
+                <div className="row">
+                    <div className="col-7">
+                        <div>
+
+                            <div title="Hospital Profile">
+                                <div className='label'>Name</div>
+                                <div className="description">Zhou Maomao</div>
+                                <div className='label'>Phones</div>
+                                <div className="description">1810000000</div>
+                                <div className='label'>Address</div>
+                                <div className="description">Hangzhou, Zhejiang</div>
+                                <div className='label'>Speciality</div>
+                                <div className="description">empty</div>
+                                <div className='label'>Emails</div>
+                                <div className="description">Lorem ipsum dolor sit amet.</div>
+                                <div className='label'>Description</div>
+                                <div className='description'>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Est dolor fuga quia quos iste? Ipsum magni nobis debitis perspiciatis. Minima, ex ullam. Ipsum maiores, tempora voluptatem distinctio blanditiis illo veniam?
+                            </div>
+                            </div>
+                        </div>
+                        <div className="text-center mt-5">
+                            <Button type='primary'>Edit</Button>
+                        </div>
+                    </div>
+                    <div className="col-5">
+                        <img className='w-100' src={require('../assets/placeholder.png')} />
+                        <div className="text-center mt-5">
+                            <Button type='primary'>new img</Button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+        return (
+            this.state.edit ? editdiv : noneditdiv
         )
     }
 }
