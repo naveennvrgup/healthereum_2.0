c0a8ac23cbdc80ee5a9dc83e7407c3269d7a5466
Shritesh <shritesh.sj@gmail.com>
2020-01-25 20:49:47 +0530

    Deployed on matic

--- frontend/web/src/blockchain/app.js
+++ frontend/web/src/blockchain/app.js
@@ -1,8 +1,8 @@
 import web3 from './web3';
-import MedicalRecord from './build/contracts/MedicalRecord.json';
+const abi = require('./build/contracts/abi.json');
 
 const  instance = new web3.eth.Contract(
-    MedicalRecord.abi,
-    '0x6d85A15EDd39D45d6769d199135D1346A8249B05'
+    abi,
+    '0x3667DE6579932Eb197427c222488244664e2Bb8B'
 );
 export default instance;
--- frontend/web/src/blockchain/web3.js
+++ frontend/web/src/blockchain/web3.js
@@ -6,7 +6,7 @@ if(typeof window !== 'undefined' && typeof  window.web3 !== 'undefined'){
 }
 else {
     const provider = new Web3.providers.HttpProvider(
-        'https://rinkeby.infura.io/v3/7b81a4617bdb4456af692497c074ccc3'
+        'https://testnet2.matic.network'
     );
     web3 = new Web3(provider);
 }
