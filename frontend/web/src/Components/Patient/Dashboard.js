4aad5d4b61784e520d14417f74239e36ebf5c80e
naveennvrgup <naveennvrgup@gmail.com>
2020-01-26 09:44:17 +0530

    user details done

--- frontend/web/src/Components/Patient/Dashboard.js
+++ frontend/web/src/Components/Patient/Dashboard.js
@@ -6,7 +6,7 @@ import web from '../../blockchain/web3'
 
 export default class Dashboard extends Component {
     state = {
-        detail:null
+        detail: []
     }
 
     componentDidMount() {
@@ -17,53 +17,62 @@ export default class Dashboard extends Component {
         const acc = web.eth.getAccounts()
         const user = constants.getaccount()
 
-        // app.methods.
+        let obj = []
+        await app.methods.getPatient(user.id).call().then(d => {
+            for (let x in d) {
+                obj.push(d[x])
+            }
+        })
+
+        await app.methods.getPatientExtra(user.id).call().then(d => {
+            for (let x in d) {
+                obj.push(d[x])
+            }
+        })
+        console.log(obj)
+        this.setState({ detail: obj })
     }
-    
+
 
     render() {
         return (
             <div>
                 <div className="d-flex justify-content-between align-items-center">
-                    <h3>User details:</h3>
-                    <button
-                        onClick={() => this.props.history.push('/patient/new/')}
-                        className="btn btn-primary">Edit profile</button>
 
                     <div className="row">
-                        <div className="col-7">
+                        <div className="col-8">
                             <div>
+                                <div className="d-flex justify-content-between">
+                                    <h3>User details:</h3>
+                                    <button
+                                        onClick={() => this.props.history.push('/patient/new/')}
+                                        className="btn btn-primary">Edit profile</button>
+                                </div>
 
                                 <div title="Hospital Profile">
-                                    <div className='label'>Name</div>
-                                    <div className="description">{this.state.detail[0]}</div>
-                                    <div className='label'>Phones</div>
-                                    <div className="description">{this.state.detail[2]}</div>
-                                    <div className='label'>Address</div>
+                                    <div className='label'>DOB</div>
                                     <div className="description">{this.state.detail[1]}</div>
-                                    <div className='label'>Speciality</div>
+                                    <div className='label'>Oragan donor</div>
+                                    <div className="description">{this.state.detail[2]}</div>
+                                    <div className='label'>Blood type</div>
                                     <div className="description">{this.state.detail[3]}</div>
-                                    <div className='label'>Emails</div>
+                                    <div className='label'>Height</div>
                                     <div className="description">{this.state.detail[4]}</div>
-                                    <div className='label'>Description</div>
-                                    <div className='description'>
-                                        {this.state.detail[5]}
-                                    </div>
-                                    <div className='label'>Hospital Type</div>
-                                    <div className='description'>
-                                        {this.state.detail[6] == 0 ? 'Government' : 'Private'}
-                                    </div>
+                                    <div className='label'>Weight</div>
+                                    <div className="description">{this.state.detail[5]}</div>
+                                    <div className='label'>Medical Conditions</div>
+                                    <div className="description">{this.state.detail[6]}</div>
+                                    <div className='label'>Allergies</div>
+                                    <div className="description">{this.state.detail[7]}</div>
+                                    <div className='label'>Important notes</div>
+                                    <div className="description">{this.state.detail[9]}</div>
+                                    <div className='label'>Medications</div>
+                                    <div className="description">{this.state.detail[8]}</div>
 
                                 </div>
                             </div>
-                            <div className="text-center mt-5">
-                                <button
-                                    className='btn btn-primary'
-                                    onClick={() => this.props.history.push('/hospital/new/')}
-                                    type='primary'>Edit</button>
-                            </div>
                         </div>
-                        <div className="col-5">
+                        <div className="col-4">
                             <img className='w-100' src={require('../assets/placeholder.png')} />
                             <div className="text-center mt-5">
                                 <button type='primary'>new img</button>
